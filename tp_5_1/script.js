478188eabcae466ef505ac2c0130c2bc813a7fae
redouane benbrahim <benbrahimhotmail2@gmail.com>
2025-05-07 18:15:38 +0100

    fixing some ereurs

--- tp_5_1/script.js
+++ tp_5_1/script.js
@@ -1,15 +1,15 @@
-let num1 = parseFloat(prompt("Entrez le premier nombre :"));
-let num2 = parseFloat(prompt("Entrez le deuxième nombre :"));
+let a = parseFloat(prompt("Entrez le premier nombre :"));
+let b = parseFloat(prompt("Entrez le deuxième nombre :"));
 
-if (isNaN(num1) || isNaN(num2)) {
+if (isNaN(a) || isNaN(b)) {
   console.log("Veuillez entrer des nombres valides.");
 } else {
-  console.log("Somme :", num1 + num2);
-  console.log("Différence :", num1 - num2);
-  console.log("Produit :", num1 * num2);
+  console.log("Somme :", a + b);
+  console.log("Différence :", a - b);
+  console.log("Produit :", a * b);
   
-  if (num2 !== 0) {
-    console.log("Quotient :", num1 / num2);
+  if (b !== 0) {
+    console.log("Quotient :", a / b);
   } else {
     console.log("Impossible de diviser par zéro.");
   }
